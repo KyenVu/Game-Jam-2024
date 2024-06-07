04bff5893ce500b16a3a790053ad6c87581c0536
nguyenvu2801 <86769475+nguyenvu2801@users.noreply.github.com>
2024-06-07 22:14:26 +0700

    Movement

---
+++ Game Jam 2024/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D m_rigidbody;
+    private Vector2 position;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = Input.GetAxis("Horizontal");
+        position.y = Input.GetAxis("Vertical");
+        if(position.magnitude >1 ) { position = position.normalized; }
+       
+    }
+    private void FixedUpdate()
+    {
+        
+        m_rigidbody.velocity = position * speed;
+    }
+}
+
+
+
+    
+

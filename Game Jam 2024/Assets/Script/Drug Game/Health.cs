14e51bebc85ec94473fadc983c8f9effa3c0e1b3
KyenVu <KyenVu264@gmail.com>
2024-06-09 00:44:26 +0700

    Unfinnish animation, finish progressbar
    Add color to progressbar

---
+++ Game Jam 2024/Assets/Script/Drug Game/Health.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Import the UI namespace for using Slider
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float CurrentHealth { get; private set; }
+
+    [SerializeField] private Slider progressBar; // Reference to the UI slider
+    [SerializeField] private float maxProgress = 100f; // Maximum value for the progress bar
+    private float currentProgress = 0f; // Current progress value
+    [SerializeField] private float medicineProgressIncrease = 10f; // Value to increase progress bar by
+
+    private void Awake()
+    {
+        CurrentHealth = startingHealth;
+        if (progressBar != null)
+        {
+            progressBar.maxValue = maxProgress;
+            progressBar.value = currentProgress;
+        }
+    }
+
+    public void TakeDamage(float damage)//receive damage and play animation
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth - damage, 0, startingHealth);
+    }
+
+    public void GetHealth(int health)
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth + health, 0, startingHealth);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("drug"))
+        {
+            TakeDamage(1.0f);
+            Debug.Log(CurrentHealth);
+        }
+
+        if (collision.CompareTag("medicine"))
+        {
+            UpdateProgress(medicineProgressIncrease);
+            Debug.Log("Progress: " + currentProgress);
+        }
+    }
+
+    private void UpdateProgress(float amount)
+    {
+        currentProgress = Mathf.Clamp(currentProgress + amount, 0, maxProgress);
+        if (progressBar != null)
+        {
+            progressBar.value = currentProgress;
+        }
+
+        if (currentProgress >= maxProgress)
+        {
+            // Handle full progress bar (e.g., level up, special ability, etc.)
+            Debug.Log("Progress bar is full!");
+            // Add your custom logic here
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Game Jam 2024/Assets/Script/Player/PlayerMovement.cs
+++ Game Jam 2024/Assets/Script/Player/PlayerMovement.cs
@@ -9,18 +9,32 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 position;
     public float speed;
 
+    private Animator animator;
+    private Vector2 lastMoveDirection;
+    private bool facingLeft = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         m_rigidbody = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+    }
+    private void FixedUpdate()
+    {
+        m_rigidbody.velocity = position * speed;
+    }
+
+    private void ProcessInputs()
+    {
         position.x = Input.GetAxis("Horizontal");
         position.y = Input.GetAxis("Vertical");
-        if(position.magnitude >1 ) { position = position.normalized; }
+        if (position.magnitude > 1) { position = position.normalized; }
         // Rotate the player to face the direction of movement
         if (position != Vector2.zero)
         {
@@ -28,10 +42,8 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         }
     }
-    private void FixedUpdate()
-    {
-        m_rigidbody.velocity = position * speed;
-    }
+
+
 }
 
 

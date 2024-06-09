012589e13b75e8e21322b03e8c3c1fae416b8e1b
Davetheduck03 <156407317+Davetheduck03@users.noreply.github.com>
2024-06-09 13:53:11 +0700

    okay

---
+++ Game Jam 2024/Assets/Script/Drug Game/timer.cs
@@ -0,0 +1,28 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    private bool isCompleted = false;
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField]float remainingTime;
+    private void Update()
+    {
+        if(remainingTime > 0) { remainingTime -= Time.deltaTime; }
+        if(remainingTime < 0) 
+        { 
+            remainingTime = 0;
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+
+        }
+        
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+    }
+}
